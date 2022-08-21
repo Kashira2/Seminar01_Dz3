a2b80b5c3991d7be64d831a489ed8eb8719b7c8f
Kashira2 <nekit.egorov@icloud.com>
2022-08-21 15:05:41 +0300

    last commit

---
+++ dz3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+if (0 == N % 2)
+{
+    Console.WriteLine(N +  " является чётным числом.");
+}
+
+else
+{
+    Console.WriteLine(N + " является нечётным числом.");
+}
